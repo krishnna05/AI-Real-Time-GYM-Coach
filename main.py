f4ab6f1fc466ebefb618f33128e07d9dd8e38c21
Krishna Vishwakarma <krishhnna02@gmail.com>
2026-09-20 08:47:17 +0530

    Page icon and Sql setup

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import streamlit as st
+import os
+import time
+import pandas as pd
+from services.auth.login_wall import render_login_wall
 
-st.set_page_config(
-    page_title="AI Real-Time Gym Coach",
-    layout="wide"
-)
+def main():
+    st.set_page_config(
+        page_icon="🏋️‍♀️",
+        page_title="AI Real-time GYM Coach",
+        initial_sidebar_state="expanded",
+        layout="centered"
+    )
 
-st.title("AI Real-Time Gym Coach")
-st.write("Setup done!")
+    load_css(os.path.join(os.getcwd(), "static", "style.css"))
+    inject_local_font(os.path.join(os.getcwd(), "static", "AdobeClean.otf"), "AdobeClean")
+
+    init_db()
+
+    if not render_login_wall():
+        return 
+
+    initial_session_defaults()
